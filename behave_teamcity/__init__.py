427ac649980dc4f2ff827a614ae450d9da9c76c2
ramonfuguet <ramon.fuguet@gmail.com>
2017-03-21 17:20:50 +0100

    Update __init__.py

--- behave_teamcity/__init__.py
+++ behave_teamcity/__init__.py
@@ -32,7 +32,9 @@ class TeamcityFormatter(Formatter):
         self.current_step = step
 
     def result(self, step_result):
-        self.msg.message('step: '+step_result.name)
+        self.msg.message('testFinished', name=step_result.name,
+                             duration=str(self.current_scenario.duration), flowId=None)
+        
         if self.current_scenario.status == "untested":
             return
 
